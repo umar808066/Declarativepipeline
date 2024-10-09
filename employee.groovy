d49a4c04d4dccf682495c1d6e9cdeb158ee034c3
umar808066 <sk.umarriya66@gmail.com>
2024-10-09 11:33:00 +0530

    c

---
+++ employee.groovy
@@ -0,0 +1,38 @@
+pipeline {
+      agent{
+        label 'dummy'    //mention the label of your webserver node provided in jenkins
+    }
+    stages {
+          stage('Pull') {
+            steps {
+                 echo "we are pulling from github"
+                 git "https://github.com/umar808066/EMPLOYEES-.git"
+            }
+        }
+        stage('Build') {
+            steps {
+                sh '''
+                sudo apt update
+                sudo apt install -y nodejs npm
+                sudo npm install -g n
+                sudo n 14.17.0 node -v0
+                sudo apt-get install git-all -y'''
+            }
+        }
+        stage('Test') {
+            steps {
+                sh '''echo "we are testing"
+                '''
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh '''
+                npm install
+                npm start 
+                echo "we are deploying"
+                '''
+            }
+        }
+    }
+}
